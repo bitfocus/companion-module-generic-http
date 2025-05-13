31e9358ab288af1117c4be010c1136b8e433effe
Per Røine <per@bitfocus.io>
2025-05-14 00:14:42 +0200

    Update config.js
    Remove HTML in config fields

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ export const configFields = [
 		width: 12,
 		label: 'Information',
 		value:
-			"<strong>PLEASE READ THIS!</strong> Generic modules is only for use with custom applications. If you use this module to control a device or software on the market that more than you are using, <strong>PLEASE let us know</strong> about this software, so we can make a proper module for it. If we already support this and you use this to trigger a feature our module doesnt support, please let us know. We want companion to be as easy as possible to use for anyone.<br /><br />Use the 'Base URL' field below to define a starting URL for the instance's commands: e.g. 'http://server.url/path/'.  <b>This field will be ignored if a command uses a full URL.</b>",
+			"PLEASE READ THIS! Generic modules is only for use with custom applications. If you use this module to control a device or software on the market that more than you are using, PLEASE let us know about this software, so we can make a proper module for it. If we already support this and you use this to trigger a feature our module doesnt support, please let us know. We want companion to be as easy as possible to use for anyone.Use the 'Base URL' field below to define a starting URL for the instance's commands: e.g. 'http://server.url/path/'.  This field will be ignored if a command uses a full URL.",
 	},
 	{
 		type: 'textinput',
@@ -27,21 +27,21 @@ export const configFields = [
 		id: 'rejectUnauthorizedInfo',
 		width: 12,
 		value: `
-					<hr />
-					<h5>WARNING</h5>
+					
+					WARNING
 					This module rejects server certificates considered invalid for the following reasons:
-					<ul>
-						<li>Certificate is expired</li>
-						<li>Certificate has the wrong host</li>
-						<li>Untrusted root certificate</li>
-						<li>Certificate is self-signed</li>
-					</ul>
-					<p>
+					
+						Certificate is expired
+						Certificate has the wrong host
+						Untrusted root certificate
+						Certificate is self-signed
+					
+					
 						We DO NOT recommend turning off this option. However, if you NEED to connect to a host
-						with a self-signed certificate you will need to set <strong>Unauthorized Certificates</strong>
-						to <strong>Accept</strong>.
-					</p>
-					<p><strong>USE AT YOUR OWN RISK!<strong></p>
+						with a self-signed certificate you will need to set Unauthorized Certificates
+						to Accept.
+					
+					USE AT YOUR OWN RISK!
 				`,
 	},
 	{
