008ad2366421f751873e86ec36d6d316a4a07ed9
AlZett <166804602+brimbolex@users.noreply.github.com>
2025-12-21 03:49:43 +0100

    Update configuration for NeoL ePowerSwitch 4

--- config.js
+++ config.js
@@ -3,56 +3,33 @@ export const configFields = [
 		type: 'static-text',
 		id: 'info',
 		width: 12,
-		label: 'Information',
+		label: 'NeoL ePowerSwitch 4',
 		value:
-			"<strong>PLEASE READ THIS!</strong> Generic modules is only for use with custom applications. If you use this module to control a device or software on the market that more than you are using, <strong>PLEASE let us know</strong> about this software, so we can make a proper module for it. If we already support this and you use this to trigger a feature our module doesnt support, please let us know. We want companion to be as easy as possible to use for anyone.<br /><br />Use the 'Base URL' field below to define a starting URL for the instance's commands: e.g. 'http://server.url/path/'.  <b>This field will be ignored if a command uses a full URL.</b>",
+			'Configure the connection to your NeoL ePowerSwitch 4. Status is read from the hidden status page and actions are sent via the hidden control interface.',
 	},
 	{
 		type: 'textinput',
 		id: 'prefix',
 		label: 'Base URL',
+		tooltip: 'Example: http://10.3.0.235:2550',
 		width: 12,
 		default: '',
 	},
 	{
 		type: 'textinput',
-		id: 'proxyAddress',
-		label: 'Proxy Address',
-		tooltip: 'E.g. http://username:password@proxy-server:8080',
+		id: 'hiddenPath',
+		label: 'Hidden page path',
+		tooltip: 'Default: /hidden.htm',
 		width: 12,
-		default: '',
+		default: '/hidden.htm',
 	},
 	{
-		type: 'static-text',
-		id: 'rejectUnauthorizedInfo',
-		width: 12,
-		value: `
-					<hr />
-					<h5>WARNING</h5>
-					This module rejects server certificates considered invalid for the following reasons:
-					<ul>
-						<li>Certificate is expired</li>
-						<li>Certificate has the wrong host</li>
-						<li>Untrusted root certificate</li>
-						<li>Certificate is self-signed</li>
-					</ul>
-					<p>
-						We DO NOT recommend turning off this option. However, if you NEED to connect to a host
-						with a self-signed certificate you will need to set <strong>Unauthorized Certificates</strong>
-						to <strong>Accept</strong>.
-					</p>
-					<p><strong>USE AT YOUR OWN RISK!<strong></p>
-				`,
-	},
-	{
-		type: 'dropdown',
-		id: 'rejectUnauthorized',
-		label: 'Unauthorized Certificates',
+		type: 'number',
+		id: 'statusPollInterval',
+		label: 'Status poll interval (ms)',
 		width: 6,
-		default: true,
-		choices: [
-			{ id: true, label: 'Reject' },
-			{ id: false, label: 'Accept - Use at your own risk!' },
-		],
+		min: 250,
+		max: 60000,
+		default: 1000,
 	},
 ]
